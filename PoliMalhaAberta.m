eb18fd2c002651cae720a91ce4583663096f1148
Lipe Lima Carmel <lipecarmel.eq@gmail.com>
2021-06-02 23:45:01 -0300

    Simulink e Ajustes
    PoliMalhaAberta simula um degrau em 15% do valor estaiconário.
    Adicionado modelo em Simulink para testes.

--- PoliMalhaAberta.m
+++ PoliMalhaAberta.m
@@ -6,15 +6,16 @@ reator = ReatorPolimer;
 u = [108, 471.6];
 y0 = [6.6832e-2, 3.3245, 323.56, 305.17, 2.7547e-4, 16.110];
 
+u = [108, 471.6];
+u(1) = 1.15*u(1);
 % Alteração de exemplo: degrau em Qi
 u = [208, 471.6];
 y0 = [6.6832e-2, 3.3245, 323.56, 305.17, 2.7547e-4, 16.110];
 
 
 [t,y] = ode15s(@(t,y) reator.derivadas(t,y,u), [0: .1 : 200], y0);
-%%
 
-            %X = [I M T Tc D0 D1]
+%% X = [I M T Tc D0 D1]
 figure
 plot(t,y(:,1))
 ylabel('Iniciador')
