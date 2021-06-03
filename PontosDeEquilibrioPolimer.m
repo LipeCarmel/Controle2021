6f9b8b8cd30cae1afadbed5b9e92822095654fe7
Lipe Lima Carmel <lipecarmel.eq@gmail.com>
2021-06-02 23:08:13 -0300

    Pontos de Equilíbrio
    Código alterado para facilitar testes entre Qc e Qi.

--- PontosDeEquilibrioPolimer.m
+++ PontosDeEquilibrioPolimer.m
@@ -4,17 +4,20 @@ reator = ReatorPolimer;
 % Ponto de Equilíbrio de Referência
 u = [108, 471.6];
 y = [6.6832e-2, 3.3245, 323.56, 305.17, 2.7547e-4, 16.110];
+
 % Variando a entrada
-Qc_test = 100 : 10 : 700;
+Q_test = 100 : 10 : 700;
+%Q_test = 50 : 2 : 200;
 
 X = [];    % Matriz de pontos de equilíbrio
 s = [];    % Nota de estabilidade
 
 wb = waitbar(0, 'Calculando...');   % Barra de carregamento
-n_test = length(Qc_test);
+n_test = length(Q_test);
 for i = 1 : n_test
     waitbar(i/n_test, wb)       % Atualizando a barra
-    uss = [u(1), Qc_test(i)];   % Alterando a entrada
+    uss = [u(1), Q_test(i)];   % Alterando a entrada
+    %uss = [Q_test(i), u(2)];   % Alterando a entrada
     
     % Buscando pontos de equilíbrio próximos à entrada
     [y,~,flag] = fsolve(@(x) reator.derivadas(0,x, uss), y,...
@@ -22,7 +25,7 @@ for i = 1 : n_test
             
     % Se um ponto de equilíbrio for encontrado
     if flag > 0
-       X = [X; [y, Qc_test(i)]];        % Armazena informação
+       X = [X; [y, Q_test(i)]];        % Armazena informação
        g = ModeloLinear(y, u, reator);  % Linearizando no ponto
        
        % A estabilidade é avaliada pela presença de apenas polos com parte 
@@ -34,7 +37,7 @@ end
 close(wb)   % Fecha barra de carregamento
 
 %% Pontos de Equilibrio
-Qc = X(:, end);
+Q = X(:, end);
 I = X(:,1);
 M = X(:,2);
 T = X(:,3);
@@ -44,24 +47,29 @@ D1 = X(:,6);
 visc = 0.0012*(D0./D1).^0.71;
 
 figure
-plot(Qc, I, '.')
+plot(Q, I, '.')
 ylabel('Iniciador')
-xlabel('Qc')
+xlabel('Q')
 
 figure
-plot(Qc, M, '.')
+plot(Q, M, '.')
 ylabel('Monômero')
-xlabel('Qc')
+xlabel('Q')
 
 figure
-plot(Qc, T, '.')
+plot(Q, T, '.')
 ylabel('Temperatura')
-xlabel('Qc')
+xlabel('Q')
 
 figure
-plot(Qc, visc, '.')
+plot(Q, Tc, '.')
+ylabel('Temperatura da Camisa')
+xlabel('Q')
+
+figure
+plot(Q, visc, '.')
 ylabel('Viscosidade')
-xlabel('Qc')
+xlabel('Q')
 
 function g = ModeloLinear(xss, u, reator)
 % Número de estados e entradas
