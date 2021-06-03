6385153d15a5c4512571da746f36edf1887f9f7b
Lipe Lima Carmel <lipecarmel.eq@gmail.com>
2021-06-02 21:59:41 -0300

    Correções e Melhoria (POO)
    Adaptação do código para POO e correção do objeto.

--- PoliMalhaAberta.m
+++ PoliMalhaAberta.m
@@ -1,32 +1,32 @@
-clear all
-close all
-clc
+clear all; close all; clc
+% Criando o reator
+reator = ReatorPolimer;
 
+% Ponto de equilíbrio
 u = [108, 471.6];
 y0 = [6.6832e-2, 3.3245, 323.56, 305.17, 2.7547e-4, 16.110];
 
+% Alteração de exemplo: degrau em Qi
 u = [208, 471.6];
 y0 = [6.6832e-2, 3.3245, 323.56, 305.17, 2.7547e-4, 16.110];
 
 
-[t,y] = ode15s(@(t,y) derivadas(t,y,u), [0: .1 : 200], y0);
+[t,y] = ode15s(@(t,y) reator.derivadas(t,y,u), [0: .1 : 200], y0);
 %%
 
             %X = [I M T Tc D0 D1]
 figure
 plot(t,y(:,1))
+ylabel('Iniciador')
 figure
 plot(t,y(:,2))
+ylabel('Monômero')
 figure
 plot(t,y(:,3))
+ylabel('Temperatura')
 figure
 plot(t,y(:,4))
+ylabel('Temperatura da Camisa')
 figure
 plot(t,(y(:,end-1)./y(:,end)).^0.71)
-%%
-% subplot(311)
-% plot(t,y(:,end-1)./y(:,end),'k-');
-% subplot(312)
-% plot(t,y(:,end-1));
-% subplot(313)
-% plot(t,y(:,end))
+ylabel('Viscosidade')
